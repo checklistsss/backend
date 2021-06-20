d8dfc0d96877dbd24692f8941e99643c61d5d853
Thiago Bentes <49199839+ThiagoNPB@users.noreply.github.com>
2021-06-20 19:47:03 -0300

    Add validation body on `CreateListPayload`

--- src/domain/interfaces/CreateList.dto.ts
+++ src/domain/interfaces/CreateList.dto.ts
@@ -1,8 +1,14 @@
+import { IsString, IsDefined, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateListPayload {
   @ApiProperty({
     example: 'My Groceries List',
   })
+  @IsDefined({
+    message: 'title is required'
+  })
+  @IsNotEmpty()
+  @IsString()
   title: string
 }
