426eddf236ba774805e8f145e17d214e27ad5578
Renato Gama <renatoargh@gmail.com>
2021-06-21 13:13:41 +0200

    Improve CreateItemPayload definition

--- src/domain/interfaces/CreateItem.dto.ts
+++ src/domain/interfaces/CreateItem.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsOptional, IsString } from 'class-validator'
 import { ItemStatus } from '../models/ItemStatus'
 
 export class CreateItemPayload {
   @ApiProperty({
     example: 'Buy spaguetti',
   })
+  @IsString()
   description: string
 
   @ApiProperty({
     enum: ItemStatus,
     example: ItemStatus.DONE,
+    default: ItemStatus.TO_DO,
+    required: false,
   })
+  @IsOptional()
+  @IsString()
   status: ItemStatus
 }
